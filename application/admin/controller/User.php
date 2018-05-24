45c4fbf6ebce419be8bc2b891f4e10903696f5c5
your name <your email>
2018-05-24 17:17:49 +0800

    没有弹出框的

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -9,8 +9,19 @@ class User extends Common{
             ->where('status',0)
             ->order('id desc')
             ->select();
-
         $this->assign('info', $info);
         return $this->fetch();
+    }
+
+    public function update(){
+
+        if($_POST){
+echo 434433;die;
+            return json_encode(array('err_msg'=>'新增成功','err_code'=>200,'success'=>'success'));
+            exit();
+
+        }
+
+        return $this->fetch('create_update');
     }
 }
